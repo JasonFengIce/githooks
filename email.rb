360f218d8aeeb5bc8694f968f6968d3164558caa
fenghuibin <fenghuibin@ismartv.cn>
2016-05-06 16:24:17 +0800

    teset

---
+++ email.rb
@@ -0,0 +1,31 @@
+require 'java'
+require './libs/Email.jar'
+include_class "javax.swing.JFrame"
+include_class "javax.swing.JLabel"
+include_class "org.apache.commons.mail.MultiPartEmail"
+include_class "org.apache.commons.mail.DefaultAuthenticator"
+
+frame = JFrame.new()
+jlabel = JLabel.new("Hello from JRuby with Swing")
+frame.getContentPane().add(jlabel)
+# frame.content_pane.add(label)
+frame.pack()
+frame.setVisible(true)
+frame.visible = true
+
+email = MultiPartEmail.new()
+email.setHostName("smtp.qiye.163.com");
+email.setSslSmtpPort("25");
+email.setAuthenticator(DefaultAuthenticator.new("fenghuibin@ismartv.cn", "Hope0Dies"));
+email.setSSLOnConnect(true);
+email.setFrom("fenghuibin@ismartv.cn");
+email.setSubject("subject");
+email.setMsg("msg from ruby");
+email.addTo("fenghuibin@ismartv.cn");
+email.send();
+
+
+
+
+
+
